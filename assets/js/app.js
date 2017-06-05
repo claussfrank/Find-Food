604c763e774c07fb470783b2f4ac4e6ab225ec4b
Clauss Franco <you@example.com>
2017-06-05 14:45:28 -0500

    Maquetación

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+var cargarPagina = function(){
+    
+     
+};
+function initMap() {
+  var uluru = {lat: 19.417575, lng: -99.164701};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+
+
+
+
+$(document).ready(cargarPagina);
